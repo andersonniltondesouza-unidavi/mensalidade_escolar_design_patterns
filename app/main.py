1bd751bc6d238eca7fc054ea332c2dae57ec74c4
andersonniltondesouza-unidavi <anderson.souza@unidavi.edu.br>
2025-11-23 13:18:30 -0300

    commit

--- app/main.py
+++ app/main.py
@@ -9,33 +9,36 @@ from infra.config import SystemConfig
 
 def main():
     config = SystemConfig()
-    print(f"--- Sistema {config.school_name} ---")
     
-    # Setup
     gerador = GeradorBoleto()
     gerador.add_observer(SistemaLogNotifier())
     gerador.add_observer(EmailNotifier("pais_do_anderson@email.com"))
 
     while True:
-        print("\n1. Gerar Boleto Normal (Sem Desconto)")
+        print(f"\n--- Sistema {config.school_name} ---")
+        print("1. Gerar Boleto Normal (Sem Desconto)")
         print("2. Gerar Boleto Aluno Mérito (20% off)")
         print("3. Gerar Boleto com Atraso (Multa + Juros)")
         print("0. Sair")
+        
+        print("------------------------------------------------")
+        print("Desenvolvido por: Anderson Nilton de Souza")
+        print("------------------------------------------------")
+
         op = input("Opção: ")
 
+        print("\n--- Resultado ---") 
         if op == '1':
             gerador.processar_mensalidade(1000.0, SemDesconto(), atrasado=False)
         elif op == '2':
             gerador.processar_mensalidade(1000.0, BolsaMerito(), atrasado=False)
         elif op == '3':
-            # Exemplo de integração: Mérito mas pagou atrasado
             gerador.processar_mensalidade(1000.0, BolsaMerito(), atrasado=True)
         elif op == '0':
+            print("Encerrando sistema...")
             break
-    
-    print("\n------------------------------------------------")
-    print("Desenvolvido por: Anderson Nilton de Souza")
-    print("------------------------------------------------")
+        else:
+            print("Opção inválida!")
 
 if __name__ == "__main__":
     main()
